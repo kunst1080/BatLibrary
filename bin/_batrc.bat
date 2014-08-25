d4ba407706b864aaf7d5c8ecf24b6713e1c76304
kunst1080 <kontrapunkt1080@gmail.com>
2014-08-25 13:00:53 +0900

    move PATH setup to /etc

--- bin/_batrc.bat
+++ bin/_batrc.bat
@@ -4,14 +4,12 @@
 @set LOCAL=%CD%
 @popd
 
-@rem 環境変数の追加
-@set PATH=%PATH%;%LOCAL%\bin
-@set PATH=%PATH%;%LOCAL%\bin-scripts
-@set PATH=%PATH%;%LOCAL%\bin-UNIX
-@set PATH=%PATH%;%LOCAL%\tools
-@rem set PATH=%PATH%;%LOCAL%\lnk
-@set PATHEXT=%PATHEXT%;.URL
-@set EDITOR=sakura
+@rem ユーザ別設定
+@set USERCONF=%LOCAL%\etc\%USERNAME%\user.init.bat
+@if exist "%USERCONF%" Call "%USERCONF%"
+
+@rem 作業フォルダの設定
+@if "%WORK%" == "" set WORK=%HOME%
 
 @if "%~1" == "--terminal" (
 	rem 端末初期設定
@@ -19,5 +17,6 @@
 	title BAT-Term
 	prompt [$S$P$S]$_$$$S
 	if exist "%LOCAL%\etc\%USERNAME%\batrc.bat" call "%LOCAL%\etc\%USERNAME%\batrc.bat"
-	cd /d "%HOME%"
+	call alias --load
+	cd /d "%WORK%"
 )
